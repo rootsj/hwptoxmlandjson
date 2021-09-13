2e9bc8a4615e14668cff64de43067ec7707e6594
rootsj <rootsj73@gmail.com>
2021-09-13 16:41:39 +0900

    09/13

--- test.py
+++ test.py
@@ -1,33 +1,39 @@
 import sys
+from collections import deque
 sys.stdin = open('test.txt', 'r')
 
 # n = int(input())
 # arr=list(map(int, input().split()))
 
-lst = input()
-stack = []
-res = ''
+n, k =map(int, input().split())
 
-for x in lst:
-    if x.isdecimal():
-        res += x
-    else:
-        if x == '(':
-            stack.append(x)
-        elif x == '*' or x == '/':
-            while stack and (stack[-1] == '*' or stack[-1] == '/'):
-                res+=stack.pop()
-            stack.append(x)
-        elif x == '+' or x == '-':
-            while stack and stack[-1] != '(':
-                res += stack.pop()
-            stack.append(x)
-        elif x == ')':
-            while stack and stack[-1] != '(':
-                res += stack.pop()
-            stack.pop()
-while stack:
-    res += stack.pop()
+# lst = [1] * n
 
-print(res)
-print(stack)
+# m = sum(lst)
+
+# i = 0
+# lstsum = 0
+# while m != 1 :
+#     s = i%n
+#     lstsum += lst[s] 
+#     if lstsum == k :
+#         lst[s] = 0
+#         lstsum = 0
+#         m = sum(lst)
+
+#     i += 1
+
+
+# print(lst.index(1) +1)
+
+dq = list(range(1, n+1))
+dq = deque(dq)
+
+while dq:
+    for _ in range(k-1):
+        cur = dq.popleft()
+        dq.append(cur)
+    dq.popleft()
+    if len(dq) ==1:
+        print(dq[0])
+        dq.popleft()
