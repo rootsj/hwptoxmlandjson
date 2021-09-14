099b644752951c414003b1f91031981f712c9a92
rootsj <rootsj73@gmail.com>
2021-09-14 16:40:58 +0900

    09/14

--- test.py
+++ test.py
@@ -5,35 +5,20 @@ sys.stdin = open('test.txt', 'r')
 # n = int(input())
 # arr=list(map(int, input().split()))
 
-n, k =map(int, input().split())
-
-# lst = [1] * n
-
-# m = sum(lst)
-
-# i = 0
-# lstsum = 0
-# while m != 1 :
-#     s = i%n
-#     lstsum += lst[s] 
-#     if lstsum == k :
-#         lst[s] = 0
-#         lstsum = 0
-#         m = sum(lst)
-
-#     i += 1
+dq = deque(list(input()))
+n = int(input())
 
 
-# print(lst.index(1) +1)
-
-dq = list(range(1, n+1))
-dq = deque(dq)
-
-while dq:
-    for _ in range(k-1):
-        cur = dq.popleft()
-        dq.append(cur)
-    dq.popleft()
-    if len(dq) ==1:
-        print(dq[0])
-        dq.popleft()
+for _ in range(n):
+    tmp = dq.copy()
+    a = list(input())
+    for x in a:
+        if tmp :
+            if tmp[0] == x:
+                tmp.popleft()
+        else:
+            break
+    if tmp:
+        print('NO')
+    else:
+        print('YES')
